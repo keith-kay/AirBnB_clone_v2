a0984b260861575b04066992b4d64a1c59b525c3
keith-kay <keith-kay>
2023-07-23 08:10:17 -0700

    update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,20 +4,21 @@ starts a Flask web application
 """
 
 from flask import Flask
-app = Flask(__name__)
+app = Flask('web_flask')
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """function to return Hello HBNB!"""
+def hello_route1():
+    """Return 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hello():
-    """ function to display HBNB"""
+def hello_route2():
+    """Return 'HBNB'"""
     return 'HBNB'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
 
